fbf5d003eff71e8c3933ed830e1463ba9dddf6b4
Austin Testut <austintestut@gmail.com>
2021-04-02 17:34:06 -0700

    added button for logging in and conditional rendering of login screen or profile page (broken axios CORS)

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      authenticated: false,
+      user: '',
+    };
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    axios.get('/google', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+    .then((res) => {
+      console.log(res);
+      this.setState({
+        authenticated: true,
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    })
   }
 
   render() {
-    return <div id="App">There be pirates!</div>;
+    return (
+      <div id="App">There be pirates!<br />
+        {!this.state.authenticated &&
+          <div>
+            <button onClick={this.login}>Log in with Google</button><br />
+          </div>}
+
+        {this.state.authenticated &&
+          <div>This is where the app will be, {this.state.user}</div>
+        }
+      </div>
+    );
   }
 }
 
