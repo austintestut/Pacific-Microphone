f4695376150a12501050653df464451e867af0d3
tarrinneal <tarrinneal@gmail.com>
2021-04-03 15:24:42 -0700

    installed wav recorder for testing, added component to test it with

---
+++ client/src/components/Recorder.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import axios from 'axios';
+
+class Recorder extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div> recorder</div>;
+  }
+}
+
+export default Recorder;
