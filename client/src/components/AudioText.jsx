174552503982ffe66e09b6ab71c7b23fa9b2480c
Tristan <tristanshawnjohnson@gmail.com>
2021-04-07 14:13:57 -0700

    Rendering to the page but it's sloppy

--- client/src/components/AudioText.jsx
+++ client/src/components/AudioText.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import FormData from 'form-data';
 
-const AudioText = ({ audioToText }) => {
-  return (
+const AudioText = ({ audioToText }) => (
     audioToText && (
-      <div>{audioToText.data.results[0].alternatives[0].transcript}</div>
+      <div>
+        {audioToText.data.results.map(result => (
+           <div>{result.alternatives[0].transcript}</div>
+          ))}
+      </div>
     )
   );
-};
 
 export default AudioText;
