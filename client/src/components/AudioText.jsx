d5423ccc62c2bf7a5d135e18bc38c93f7ab3ff96
Tristan <tristanshawnjohnson@gmail.com>
2021-04-07 23:09:45 -0700

    Refactored the audioToText algorithim
    Did this so questions would end in question marks, sentences with periods, and sentences would start with upper case letters

--- client/src/components/AudioText.jsx
+++ client/src/components/AudioText.jsx
@@ -5,9 +5,25 @@ import FormData from 'form-data';
 const AudioText = ({ audioToText }) => (
     audioToText && (
       <div id="audioTextDisplay">
-        {audioToText.data.results.map(result => (
-           <p>{result.alternatives[0].transcript}</p>
-          ))}
+        {audioToText.data.results.map(result => {
+          const regEx = /who|what|when|where|why|how/i;
+          let transcript = result.alternatives[0].transcript.split(' ');
+          transcript.pop();
+          let firstWord = transcript[0];
+          const firstLetter = firstWord[0].toUpperCase();
+          firstWord = firstLetter + firstWord.substring(1, firstWord.length);
+          transcript.shift();
+          transcript.unshift(firstWord);
+          transcript = transcript.join(' ');
+
+          if (regEx.test(firstWord)) {
+            transcript += '? ';
+          } else {
+            transcript += '. ';
+          }
+
+          return <p>{transcript}</p>
+        })}
       </div>
     )
   );
