b0baac05645eb0580ad0d36de544d60a679f2027
Samuel Cho <SamuelCho-ubf>
2021-04-09 15:06:58 -0700

    change play button icons and add hovering effect on buttons

--- client/src/components/LPComponents/LPPlayButtons.jsx
+++ client/src/components/LPComponents/LPPlayButtons.jsx
@@ -11,24 +11,36 @@ const LPPlayButtons = ({
   replayScript,
 }) => (
   <div className="playButtons">
-    {pointer > 0 && (
-      <button type="button" onClick={playPrevious}>
-        <GrCaretPrevious />
+    {
+    // pointer > 0 &&
+    (
+      <button type="button" className="button" onClick={playPrevious}>
+        {/* <GrCaretPrevious /> */}
+        <img src="./images/icons/146-play_.png" alt="playButton" />
       </button>
     )}
-    {pointer !== audiosLength - 1 && (
-      <button type="button" onClick={playNext}>
-        <GrCaretNext />
+    {
+    // pointer !== -1 &&
+    (
+      <button type="button" className="button" onClick={repeat}>
+        {/* <GrRefresh /> */}
+        <img src="./images/icons/153-recycle-2.png" alt="playButton"  />
       </button>
     )}
-    {pointer !== -1 && (
-      <button type="button" onClick={repeat}>
-        <GrRefresh />
+    {
+    // pointer !== audiosLength - 1 &&
+    (
+      <button type="button" className="button" onClick={playNext}>
+        {/* <GrCaretNext /> */}
+        <img src="./images/icons/146-play.png" alt="playButton"  />
       </button>
     )}
-    {pointer !== -1 && (
-      <button type="button" onClick={replayScript}>
-        <RiArrowGoBackFill />
+    {
+    // pointer !== -1 &&
+    (
+      <button type="button" className="button" onClick={replayScript}>
+        {/* <RiArrowGoBackFill /> */}
+        <img src="./images/icons/154-refresh.png"  />
       </button>
     )}
   </div>
