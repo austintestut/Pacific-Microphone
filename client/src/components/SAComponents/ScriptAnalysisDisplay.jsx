091a195e0880bb264618ed406d2198dc38de73c9
larakhdavies <lara.kh.davies@gmail.com>
2021-04-09 14:06:03 -1000

    add logic to accomadate for no . or ? pr ! to finish a sentece

--- client/src/components/SAComponents/ScriptAnalysisDisplay.jsx
+++ client/src/components/SAComponents/ScriptAnalysisDisplay.jsx
@@ -3,24 +3,32 @@ import React from 'react';
 const ScriptAnalysisDisplay = ({ script, displayWatsonAnalysis }) => {
   const { title, author, talkingBlocks, watsonAnalysis } = script;
 
-  const regex = /[\w-,;_%''""$&#@*() ]+[.?!] ?/g;
+  const regex = /[\w-,;_%''""$&#@*():{} ]+[.?!] ?/g;
 
   return (
     <div className="scriptDisplay">
       <div className="title">{title}</div>
       <div className="author">By: {author}</div>
-      {talkingBlocks.map((talkingBlock, idx) => (
+      {talkingBlocks.map((talkingBlock, idx) => {
+        let arrayOfSentences = [];
+        if (talkingBlock.text.includes(".") || talkingBlock.text.includes("?") || talkingBlock.text.includes("!")) {
+          arrayOfSentences = talkingBlock.text.match(regex)
+        } else {
+          arrayOfSentences.push(talkingBlock.text);
+        }
+        return (
         <div key={idx}>
           <div>{talkingBlock.character}</div>
-          {talkingBlock.text.match(regex).map((s, idx) => (
+          {arrayOfSentences.map((s, idx) => (
             <span key={idx}>
-              {s && (
+              {(s) && (
                 <span onClick={() => displayWatsonAnalysis(s.trim())}>{s}</span>
               )}
             </span>
           ))}
         </div>
-      ))}
+      );})
+    }
     </div>
   );
 };
